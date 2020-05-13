edd934716ec35bc2bcc871dc852c83349e3a7d60
elwiiman <emmanuel.sanro@gmail.com>
2020-05-13 00:38:57 -0500

    Basic Form component added, css added to the component via module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
-  return <h1>News</h1>;
+  return (
+    <Fragment>
+      <Header title="News Searcher"></Header>
+      <div className="container white">
+        <Form></Form>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
